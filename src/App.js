a228456b9118dabe81bbc87de4042ae802da5cce
Cinerarius <koustavrakshit10b@gmail.com>
2022-08-05 02:31:01 +0530

    main

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import { Html } from '@react-three/drei'
 
 import Button from './components/Button';
 import Particle from './components/Particle';
-import { Model } from './components/Model'
 
 function App() {
   return (
     <>
       <Canvas camera={{ position: [-0.5, 0.5, 3], fov: 30 }}>
-        <Html fullscreen>
+        <Html>
           <Button />
         </Html>
         <Particle />
-        <Model />
         <ambientLight intensity={0.1} />
       </Canvas>
     </>
   );
 }
-
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,14 @@
 import * as React from 'react'
 
 const Button = () => {
-    return(
-        <nav>
-            <button className="button">Path of SOL</button>
-            <button className="button">About Us</button>
-            <button className="button">IANUA AI</button>
-            <button className="button">Blog</button>
+    return (
+        <nav className='nav'>
+                <ul>
+                    <li><a href="#">Path of SOL</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">IANUA AI</a></li>
+                </ul>
         </nav>
     )
 }
