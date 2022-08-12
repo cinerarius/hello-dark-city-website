4e13dd964f92fa685198bbf5aaaf8cf17bdb986e
Cinerarius <koustavrakshit10b@gmail.com>
2022-08-12 10:16:01 +0530

    main

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,10 +4,10 @@ const Button = () => {
     return (
         <div className='nav'>
             <ul>
-            <button className='btn' href='#'>IANUA AI</button>
-            <button className='btn' href='#'>BLOG</button>
-            <button className='btn' href='#'>ABOUT US</button>
-            <button className='btn' href='#'>PATH OF SOL</button>
+            <button className='btn button1' href='#'>IANUA AI</button>
+            <button className='btn button2' href='#'>BLOG</button>
+            <button className='btn button3' href='#'>ABOUT US</button>
+            <button className='btn button4' href='#'>PATH OF SOL</button>
             </ul>
         </div>
     )
