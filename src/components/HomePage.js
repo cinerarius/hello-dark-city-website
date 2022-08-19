618558fe9ad3fe2e686ad145dfd382f77c8191ad
Cinerarius <koustavrakshit10b@gmail.com>
2022-08-19 19:39:30 +0530

    mian

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -12,9 +12,23 @@ import Button from './Button'
 import { Model } from './Model'
 import Particle from './Particle'
 
+
+import GreenMist1 from "../assets/Background/1-green_mist-overlay.png"
+import Sky from "../assets/Background/6-sky.gif"
+import ForegroundTrees2 from "../assets/Background/2-foreground-trees.png"
+import BackgroundTrees4 from "../assets/Background/4-background-trees.png"
+import BackgroundTrees5 from "../assets/Background/5-background-trees.png"
+
 function App() {
    return (
       <>
+         <div id="images" style={{ height: "100vh", width: "100vw", position: "absolute" }}>
+            <img src={GreenMist1} alt=""></img>
+            <img src={ForegroundTrees2} alt=""></img>
+            <img src={BackgroundTrees4} alt=""></img>
+            <img src={BackgroundTrees5} alt=""></img>
+            <gif src={Sky} alt=""></gif>
+         </div>
          <Canvas camera={{ position: [-0.5, 0.5, 3], fov: 30 }}>
             <Html>
                <Button />
